37cb31094269c147ee53b7cc1bf9337a2de75a2d
sunyoung <oolongsawel@naver.com>
2019-12-05 23:32:35 +0900

    Add defs.h for char[]
    구조체 char형 배열인 멤버변수들의 size를 상수로 #define하여 추가함
    
    기존 char * EMPNO[6]
    변경후 char * EMPNO[SIZE_EMPNO]

---
+++ temp/defs.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef _DEFS_H
+#define _DEFS_H
+
+//"Employee.h"
+#define SIZE_EMPNO			6 //[6]
+#define SIZE_NAME			12 //[12]
+#define SIZE_DEPT			3 //[3]
+#define SIZE_HIREDATE		12 //[12]
+#define SIZE_SEX			1 //[1]
+#define SIZE_KEY			100 //[100]
+
+
+#endif
